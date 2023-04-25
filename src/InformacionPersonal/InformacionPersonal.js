b1f0d5938914d305d2f5cef2ee50aa09059f105f
Juan Muñoz <contactomillerxj@gmail.com>
2023-04-24 23:25:27 -0500

    Hice proyecto en casa

---
+++ src/InformacionPersonal/InformacionPersonal.js
@@ -0,0 +1,26 @@
+export function InformacionPersonal() {
+    return (
+        <>
+            <div class="banner3">
+                <section class="container-fluid my-5">
+                    <div class="p-2"></div>
+                    <img
+                        src=""
+                        class="mx-auto d-block w-25"
+                    />
+                </section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 mx-auto text-center">
+                            <br></br>
+                            <h1 class="text-center text-black font-weight-bold mb-4">
+                            </h1>
+                            <h4 class="text-center text-black font-weight-bold mb-4">
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/QuienSoyYo/QuienSoyYo.js
@@ -0,0 +1,41 @@
+export function QuienSoyYo() {
+    return (
+        <>
+            <div class="banner2">
+                <section class="container-fluid my-5">
+                    <div class="p-2"></div>
+                    <img
+                        src="https://firebasestorage.googleapis.com/v0/b/vapes-ecfe6.appspot.com/o/logo.png?alt=media&token=3d4efc6f-7b4e-4fd9-8698-fae866094459"
+                        class="mx-auto d-block"
+                    />
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 mx-auto text-center">
+                                <br></br>
+                                <h1 class="text-center text-black font-weight-bold mb-4"></h1>
+                                <h4 class="text-center text-black font-weight-bold mb-4">
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <footer>
+                <div class="container-fluid bg-dark p-5">
+                    <div class="row">
+                        <div class="col-6 text-white text-end align-self-center">
+                            <h2>&copy; Code vapes</h2>
+                            <h4>El vape de tu preferencia a tu alcance</h4>
+                            <h5>Girardota</h5>
+                            <h5>Colombia</h5>
+
+                        </div>
+                        <div class="col-6">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/vapes-ecfe6.appspot.com/o/logo.png?alt=media&token=3d4efc6f-7b4e-4fd9-8698-fae866094459"></img>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
